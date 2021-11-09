cee56203676de52bdec278fcb89d2b65e619ac28
Kleeritter <93885297+Kleeritter@users.noreply.github.com>
2021-11-09 22:13:05 +0100

    test

--- Übung1/aufgabeb.py
+++ Übung1/aufgabeb.py
@@ -28,5 +28,35 @@ plt.plot(te,mp, te, cp)
 plt.savefig('Vergleich.png')
 plt.legend()
 
+# Aufgabe e
+cpp = np.array([])
+lvo= 2.5*10**6
+rv = 461.4
+cpv =1870
+cpi= 4187
+to = 273.15
+for t in range(233,333):
+    t = t + 0.15 #Korrektur für Temperatur
+    #dt= (t- 273.15) # Bestimmung von delta T
+    c = 611.2*math.exp((-lvo + t*(cpv-cpi)*math.log(t)+to*(cpv-cpi))/(rv*t) -(2*to*(cpv-cpi)-lvo)/(to*rv))
+    cpp=np.append(cpp,c)
+print(cpp)
+plt.plot(te,mp, te, cp,te,cpp)
+plt.savefig('Vergleich2.png')
+plt.legend()
 
-Hw = 7 
+cppp = np.array([])
+lvo= 2.5*10**6
+rv = 461.4
+cpv =1870
+cpi= 4187
+to = 273.15
+for t in range(233,333):
+    t = t + 0.15 #Korrektur für Temperatur
+    #dt= (t- 273.15) # Bestimmung von delta T
+    c = 611.2*math.exp(-1*(lvo+(cpv-cpi)*(t-to))/(rv*(t+to))+(cpv-cpi)/(rv*(t+to)))
+    cppp=np.append(cppp,c)
+print(cppp)
+plt.plot(te,mp, te, cp,te,cpp)
+plt.savefig('Vergleich2.png')
+plt.legend()

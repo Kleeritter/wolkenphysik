50cdeaba7fe73c9df1779c36d7dec4de48ca2fc9
Kleeritter <steding@tutanota.com>
2021-11-20 15:13:11 +0100

    goose

--- Übung2/Steding-Alexander-wp02.py
+++ Übung2/Steding-Alexander-wp02.py
@@ -15,8 +15,8 @@ for r in  np.arange(100*10**(-9),100*10**(-6),1*10**(-9)):
     v=3
     masse=10**(-19)
     molmasse=  0.13214
-    B= v*masse*molmasse
-    s= 1+ (A/r) -(B/r**3)
+    B= v*masse*molmasse**(-1)
+    s= 1+ (A/r) -(B/(r**3))
     amo=np.append(amo,s)
 
 #Seesalzschwer
@@ -24,8 +24,8 @@ for r in  np.arange(100*10**(-9),100*10**(-6),1*10**(-9)):
     v=2
     masse=10**(-19)
     molmasse=  0.05844 
-    B= v*masse*molmasse
-    s= 1+ (A/r) -(B/r**3)
+    B= v*masse*molmasse**(-1)
+    s= 1+ (A/r) -(B/(r**3))
     seesalzschwer=np.append(seesalzschwer,s)
 
 #Seesalzleicht
@@ -33,14 +33,15 @@ for r in  np.arange(100*10**(-9),100*10**(-6),1*10**(-9)):
     v=2
     masse=10**(-21)
     molmasse=  0.05844 
-    B= v*masse*molmasse
-    s= 1+ (A/r) -(B/r**3)
+    B= v*masse*molmasse**(-1)
+    s= 1+ (A/r) -(B/(r**3))
     seesalzleicht=np.append(seesalzleicht,s)
 
 
 plt.plot(rad,amo,label="Amoniumsulfat")
 plt.plot(rad,seesalzschwer,label="Seesalz schwer")
 plt.plot(rad,seesalzleicht,label="Seesalz leicht")
+plt.axis([10**-7,10**-4,0,2])
 plt.xscale("log")
 plt.legend()
 plt.savefig("test.png")
